efc2aabab88fc915ae0bb435ed01e3a494e78da3
RobinAxelsson <63591629+RobinAxelsson@users.noreply.github.com>
2021-03-01 15:59:11 +0100

    Update Program.cs

--- BaristaApi/Program.cs
+++ BaristaApi/Program.cs
@@ -17,3 +17,24 @@ namespace BaristaApi
         }
     }
 }
+/*
+IBeverage espresso = new FluentEspresso()
+                            .AddWater(20)
+                            .AddBeans(new Bean(){ 
+                                AmountInG = 5,
+                                Sort = CoffeSorts.Robusta})
+    						.Validate(e => e.Temerature > 90)
+                        .ToBeverage();
+// espresso is type of Espresso
+
+IBeverage latte = new FluentEspresso()
+                            .AddBeans(new Bean(){ 
+                                AmountInG = 5,
+                                Sort = CoffeSorts.Robusta})
+                            .GrindBeans()
+                            .AddWater(20)
+                            .AddMilk()
+       						.Validate(e => e.Temerature < 80)
+                        .ToBeverage();
+// latte is type of Latte
+*/
