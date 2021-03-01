4a17df38565a0fb23170e03081c0c795ee967a83
RobinAxelsson <63591629+RobinAxelsson@users.noreply.github.com>
2021-03-01 15:47:03 +0100

    first swap

--- BaristaApi/Beverage.cs
+++ BaristaApi/Beverage.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 public interface IBeverage{
 	List<string> Ingredients { get; }
     string CupType { get; }
+
+    public IBeverage AddWater();
+    public IBeverage BoilWater();
+    public IBeverage GrindBeans();
+    public IBeverage AddBeans();
+    public IBeverage AddMilk();
 }
 
 class Espresso : IBeverage
@@ -10,11 +16,62 @@ class Espresso : IBeverage
     public List<string> Ingredients => throw new System.NotImplementedException();
 
     public string CupType => throw new System.NotImplementedException();
+
+    public IBeverage AddBeans()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IBeverage AddMilk()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IBeverage AddWater()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IBeverage BoilWater()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IBeverage GrindBeans()
+    {
+        throw new System.NotImplementedException();
+    }
 }
 
 class Latte : IBeverage
 {
+
     public List<string> Ingredients => throw new System.NotImplementedException();
 
     public string CupType => throw new System.NotImplementedException();
+
+    public IBeverage AddBeans()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IBeverage AddMilk()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IBeverage AddWater()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IBeverage BoilWater()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IBeverage GrindBeans()
+    {
+        throw new System.NotImplementedException();
+    }
 }
