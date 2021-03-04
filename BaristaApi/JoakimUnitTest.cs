6b7eafb8271887774d96551fab7cb8128ddebc84
joakimwidell <joakimwidell@hotmail.com>
2021-03-04 11:34:08 +0100

    Create JoakimUnitTest.cs

---
+++ BaristaApi/JoakimUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BaristaApi.Tests
+{
+    public class FluentApiTests
+    {
+        [Fact]
+        public void When_AddingWaterAndBeans_Expect_Espresso()
+        {
+            //Pseudo code
+            /*
+            // Act
+            var beverage = new EspressoMachine().AddWater().AddBeans().ToBeverage();
+            // Assert
+            Assert.IsType<Espresso>(beverage);
+            */
+        }
+    }
+}
