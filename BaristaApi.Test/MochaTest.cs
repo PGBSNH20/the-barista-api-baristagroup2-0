60397bd8ed8d543440b1fca0ae2e8bf7c882a810
RobinAxelsson <axelsson.robin@outlook.com>
2021-03-04 14:10:57 +0100

    a mocha test

---
+++ BaristaApi.Test/MochaTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace BaristaApi.Tests
+{
+    public class MochaTest
+    {
+        [Fact]
+        public void When_AddingMilkAndChoclate_Expect_Mocha()
+        {
+            IBeverage beverage = new FluentEspresso()
+                 .FillWater()
+                 .AddBeans(new Bean() { AmountInG = 5, Sort = CoffeSort.Robusta })
+                 .PourEspresso()
+                 .AddMilk()
+                 .AddChocolate()
+                 .ToBeverage();
+
+            // Assert
+            Assert.IsType<Mocha>(beverage);
+        }
+    }
+}
